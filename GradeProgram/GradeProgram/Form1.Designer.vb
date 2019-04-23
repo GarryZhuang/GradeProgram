cfc1645c0bdbf15fc1f547dcd53bdbf1b04ef767
Garry Zhuang <34046345+GarryZhuang@users.noreply.github.com>
2019-04-23 13:55:12 +1000

    initial upload

---
+++ GradeProgram/GradeProgram/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPercentage = New System.Windows.Forms.TextBox()
+        Me.txtLetterGrade = New System.Windows.Forms.TextBox()
+        Me.btnCalculator = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Bahnschrift", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(36, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Percentage:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Bahnschrift", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(27, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 23)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Letter Grade:"
+        '
+        'txtPercentage
+        '
+        Me.txtPercentage.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPercentage.Location = New System.Drawing.Point(151, 33)
+        Me.txtPercentage.MaxLength = 3
+        Me.txtPercentage.Name = "txtPercentage"
+        Me.txtPercentage.Size = New System.Drawing.Size(100, 29)
+        Me.txtPercentage.TabIndex = 2
+        '
+        'txtLetterGrade
+        '
+        Me.txtLetterGrade.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetterGrade.Location = New System.Drawing.Point(151, 83)
+        Me.txtLetterGrade.MaxLength = 1
+        Me.txtLetterGrade.Name = "txtLetterGrade"
+        Me.txtLetterGrade.ReadOnly = True
+        Me.txtLetterGrade.Size = New System.Drawing.Size(100, 29)
+        Me.txtLetterGrade.TabIndex = 3
+        '
+        'btnCalculator
+        '
+        Me.btnCalculator.Font = New System.Drawing.Font("Microsoft YaHei", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculator.Location = New System.Drawing.Point(12, 152)
+        Me.btnCalculator.Name = "btnCalculator"
+        Me.btnCalculator.Size = New System.Drawing.Size(274, 56)
+        Me.btnCalculator.TabIndex = 4
+        Me.btnCalculator.Text = "Calculate"
+        Me.btnCalculator.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(298, 228)
+        Me.Controls.Add(Me.btnCalculator)
+        Me.Controls.Add(Me.txtLetterGrade)
+        Me.Controls.Add(Me.txtPercentage)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMain"
+        Me.Text = "GradeCalculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPercentage As TextBox
+    Friend WithEvents txtLetterGrade As TextBox
+    Friend WithEvents btnCalculator As Button
+End Class
---
+++ GradeProgram/GradeProgram/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class frmMain
+    Private Sub btnCalculator_Click(sender As Object, e As EventArgs) Handles btnCalculator.Click
+        'Use try method to prevent error's
+        Try
+            'Get the letter grade from Function
+            txtLetterGrade.Text = GetLetterGradeFromPercentage(txtPercentage.Text)
+        Catch ex As Exception
+            'There was an data error!
+            MsgBox("Data Error!")
+        End Try
+    End Sub
+
+
+    'Function to calculate the percentage
+    Public Function GetLetterGradeFromPercentage(ByVal percentage As Integer)
+
+        'Define letter grade as nothing for the start
+        Dim LetterGrade As String = ""
+
+        'Check if the percentage falls in between the data range
+        If percentage >= 90 Then
+            'define the letter grade
+            LetterGrade = "A"
+
+            txtLetterGrade.BackColor = Color.Lime
+        ElseIf percentage >= 80 Then
+            'define the letter grade
+            LetterGrade = "B"
+
+            txtLetterGrade.BackColor = Color.Lime
+        ElseIf percentage >= 70 Then
+            'define the letter grade
+            LetterGrade = "C"
+
+            txtLetterGrade.BackColor = Color.Yellow
+        ElseIf percentage >= 60 Then
+            'define the letter grade
+            LetterGrade = "D"
+
+            txtLetterGrade.BackColor = Color.Orange
+        ElseIf percentage >= 50 Then
+            'define the letter grade
+            LetterGrade = "E"
+
+            txtLetterGrade.BackColor = Color.DarkOrange
+        ElseIf percentage < 50 Then
+            'define the letter grade
+            LetterGrade = "N"
+
+            txtLetterGrade.BackColor = Color.Red
+        Else
+            'display error due to number/char's outside the range
+            LetterGrade = "ERR"
+
+            txtLetterGrade.BackColor = Color.Purple
+        End If
+
+        'retrun the data to the OG entity
+        Return LetterGrade
+    End Function
+End Class
